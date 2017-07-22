8e27a2b477e49819d4557f4494fcb9d9ad91497a
vagrant <vagrant@glon>
2017-07-23 01:34:02 +0800

    admin add userinfo

--- account/admin.py
+++ account/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, UserInfo
 
 # Register your models here.
 class UserPorfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'birth', 'phone')
     list_filter = ('phone',)
 
-admin.site.register(UserProfile, UserPorfileAdmin)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'school', 'company', 'profession', 'address', 'aboutme', 'photo')
+    list_filter = ('school', 'company', 'profession')
 
+admin.site.register(UserProfile, UserPorfileAdmin)
+admin.site.register(UserInfo, UserInfoAdmin)
 
