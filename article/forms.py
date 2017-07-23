92d2550184b982ef94170c22c882def06bcfcdd6
vagrant <vagrant@glon>
2017-07-24 00:34:09 +0800

    添加栏目管理

---
+++ article/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import ArticleColumn
+
+class ArticleColumnForm(forms.ModelForm):
+    class Meta:
+        model = ArticleColumn
+        fields = ('column',)
+
+
+
