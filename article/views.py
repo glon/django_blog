c2ff305cc3f37b3906186ccbee9ae2401d3a9742
glon <562071793@qq.com>
2017-07-26 18:06:34 +0800

    post list

--- article/views.py
+++ article/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404
 
 from .models import ArticleColumn, ArticlePost
 from .forms import ArticleColumnForm, ArticlePostForm
@@ -63,4 +64,17 @@ def article_post(request):
         article_post_form = ArticlePostForm()
         article_columns = request.user.article_column.all()
         return render(request, 'article/article_post.html', {'article_post_form':article_post_form, 'article_columns':article_columns})
+
+@login_required(login_url='account/login/')
+def article_list(request):
+    articles = ArticlePost.objects.filter(author=request.user)
+    return render(request, 'article/article_list.html', {'articles':articles})
+
+@login_required(login_url='/account/login/')
+def article_detail(request, id, slug):
+    article = get_object_or_404(ArticlePost, id=id, slug=slug)
+    return render(request, "article/article_detail.html", {"article":article})
+
+
+
 
