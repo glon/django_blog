7627f36d028fec874822ef9156189d18f6a0325d
glon <562071793@qq.com>
2017-07-28 16:40:45 +0800

    article list pagination

--- article/views.py
+++ article/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import ArticleColumn, ArticlePost
 from .forms import ArticleColumnForm, ArticlePostForm
@@ -65,10 +66,27 @@ def article_post(request):
         article_columns = request.user.article_column.all()
         return render(request, 'article/article_post.html', {'article_post_form':article_post_form, 'article_columns':article_columns})
 
-@login_required(login_url='account/login/')
+# @login_required(login_url='account/login/')
+# def article_list(request):
+#     articles = ArticlePost.objects.filter(author=request.user)
+#     return render(request, 'article/article_list.html', {'articles':articles})
+@login_required(login_url='/account/login/')
 def article_list(request):
-    articles = ArticlePost.objects.filter(author=request.user)
-    return render(request, 'article/article_list.html', {'articles':articles})
+    articles_list = ArticlePost.objects.filter(author=request.user)
+    paginator = Paginator(articles_list, 2)
+    page = request.GET.get('page')
+    try:
+        current_page = paginator.page(page)
+        articles = current_page.object_list
+    except PageNotAnInteger:
+        current_page = paginator.page(1)
+        articles = current_page.object_list
+    except EmptyPage:
+        current_page = paginator.page(paginator.num_pages)
+        articles = current_page.object_list
+
+    return render(request, "article/article_list.html", {'articles':articles, 'page':current_page})
+
 
 @login_required(login_url='/account/login/')
 def article_detail(request, id, slug):
