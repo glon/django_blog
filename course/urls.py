97004bf3915bced6be22171ee501b2a0b23abf2c
glon <562071793@qq.com>
2017-08-11 15:45:36 +0800

    class-based view and mixin

--- course/urls.py
+++ course/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import AboutView
+from .views import AboutView, CourseListView
 
 urlpatterns = [
     # url(r'about/$', TemplateView.as_view(template_name='course/about.html')),
     url(r'about/$', AboutView.as_view(), name='about'),
+    url(r'course-list/$', CourseListView.as_view(), name='course_list'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Course
+
 
 # Create your views here.
 class AboutView(TemplateView):
     template_name = "course/about.html"
+
+
+class CourseListView(ListView):
+    model = Course
+    context_object_name = 'courses'
+    template_name = 'course/course_list.html'
+
+
+class UserMixin:
+    def get_queryset(self):
+        qs = super(UserMixin, self).get_queryset()
+        return qs.filter(user=self.request.user)
+
+
+class UserCourseMixin(UserMixin):
+    module = Course
+
+
+class ManageCourseListView(UserCourseMixin, ListView):
+    context_object_name = "courses"
+    template_name = 'course/manage_course_list.html'
+
+
 
 
 
