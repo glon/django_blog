78cb92d02e3bc5b9273451de2b3abc263f30334f
何金龙 <hejinlong@yaochufa.com>
2017-09-27 14:57:09 +0800

    课程内容详情

--- course/urls.py
+++ course/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import AboutView, CourseListView, ManageCourseListView, CreateCourseView, DeleteCourseView, CreateLessonView, ListLessonView
+from .views import AboutView, CourseListView, ManageCourseListView, CreateCourseView, DeleteCourseView, CreateLessonView, ListLessonView, DetailLessonView
 
 urlpatterns = [
     # url(r'about/$', TemplateView.as_view(template_name='course/about.html')),
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'delete-course/(?P<pk>\d+)/$', DeleteCourseView.as_view(), name='delete_course'),
     url(r'create-lesson/$', CreateLessonView.as_view(), name='create_lesson'),
     url(r'list-lessons/(?P<course_id>\d+)/$', ListLessonView.as_view(), name='list_lessons'),
+    url(r'detail-lesson/(?P<lesson_id>\d+)/$', DetailLessonView.as_view(), name='detail_lesson'),
 ]
